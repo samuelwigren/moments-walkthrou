c21f903c87065e717e2ea8cc21ad644adc4b5f0c
samuelwigren <samuel.wigren@gsfacket.se>
2023-07-14 12:28:46 +0200

    Popular profile view

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -14,11 +14,8 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
-  const setCurrentUser = useSetCurrentUser();
-
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -30,10 +27,8 @@ function SignInForm() {
   const history = useHistory();
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     try {
-      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
-      setCurrentUser(data.user);
+      await axios.post("/dj-rest-auth/login/", signInData);
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
